4edf70b0429d09689f30d7f0aba6a48a02c9a079
begyyal-ghost <begyyal@gmail.com>
2021-08-06 12:24:03 +0000

    BY-CI

--- dist/index.js
+++ dist/index.js
@@ -3231,41 +3231,39 @@ const core = __nccwpck_require__(24);
 const axios = __nccwpck_require__(992);
 
 const WF_NAME = core.getInput('workflowName');
-const AWAIT_START_FLAG = core.getInput('awaitStartFlag');
-const START_STATUS = core.getInput('startStatus');
 const TIMEOUT_MSEC = core.getInput('timeoutSec') * 1000;
 const API_PATH = '/repos/' + core.getInput('repos') + '/actions/runs';
 
 const RETRY_COUNT = 10;
-const INTERVAL_SEC = AWAIT_START_FLAG ? 1 : 3;
+const INTERVAL_SEC = 3;
 
-async function workflowIsRunning(config) {
-  return AWAIT_START_FLAG
-    ? await workflowExists(config, START_STATUS).catch(err => { throw err })
-    : await workflowExists(config, 'queued').catch(err => { throw err })
-    || await workflowExists(config, 'in_progress').catch(err => { throw err });
+async function getWorkflowIds(status) {
+  const res = await request(API_PATH, newConf(status), RETRY_COUNT).catch(err => { throw err });
+  return res.data.workflow_runs
+    .filter(wfr => !WF_NAME || wfr.name == WF_NAME)
+    .map(wfr => wfr.id);
 }
 
-async function workflowExists(config, status) {
-
-  config.params.status = status;
-  const res = await request(config, RETRY_COUNT).catch(err => { throw err });
-
-  const result = res.data.total_count != 0
-    && (!WF_NAME || res.data.workflow_runs.some(wfr => wfr.name == WF_NAME));
-  console.log('Successfully received API response.');
-  console.log('The workflow run of ' + status + (result ? ' exists.' : ' doesnt exist.'));
-
-  return result;
+async function checkCompletion(ids) {
+  return (await Promise
+    .all(ids.map(id => getWorkflowStatus(id)))
+    .catch(err => { throw err }))
+    .every(s => s == 'completed');
 }
 
-async function request(config, count) {
+async function getWorkflowStatus(id) {
+  const path = API_PATH + '/' + id;
+  const res = await request(path, newConf(), RETRY_COUNT).catch(err => { throw err });
+  return res.status;
+}
 
-  const res = await axios.get(API_PATH, config).catch(err => { throw err });
+async function request(path, config, count) {
+
+  const res = await axios.get(path, config).catch(err => { throw err });
 
   if (res.status != 200)
     console.log('The http status that is response of Github REST API is not success, it is ' + res.status + '.');
-  else if (res.data.total_count != res.data.workflow_runs.length)
+  else if (res.data.total_count && res.data.total_count != res.data.workflow_runs.length)
     console.log('The response of Github REST API contains a mismatch between [total_count:' + res.data.total_count + '] and [workflow_runs.length:' + res.data.workflow_runs.length + '].');
   else
     return res;
@@ -3281,32 +3279,31 @@ function sleep(sec) {
   return new Promise((resolve) => setTimeout(resolve, sec * 1000));
 }
 
-function validate() {
-  if (AWAIT_START_FLAG && !['queued', 'in_progress'].some(s => s == START_STATUS))
-    throw 'The start status of arguments is invalid.';
-}
-
-async function run() {
-
-  const config = {
+function newConf(status) {
+  const params = status ? { status: status } : {};
+  return {
     baseURL: 'https://api.github.com/',
     headers: {
       Accept: 'application/vnd.github.v3+json',
       Authorization: 'token ' + core.getInput('token')
     },
-    params: {
-      status: ''
-    }
+    params: params
   };
+}
+
+async function run() {
+
+  const ids = (await Promise
+    .all(['queued', 'in_progress'].map(s => getWorkflowIds(s)))
+    .catch(err => { throw err }))
+    .flatMap();
+  if (ids.length == 0)
+    return;
 
   let timeoutFlag = false;
   const start = new Date();
 
-  while ((AWAIT_START_FLAG
-    ? !(await workflowIsRunning(config).catch(err => { throw err }))
-    : await workflowIsRunning(config).catch(err => { throw err }))
-    && !timeoutFlag) {
-
+  while (!(await checkCompletion(ids).catch(err => { throw err })) && !timeoutFlag) {
     await sleep(INTERVAL_SEC)
     timeoutFlag = new Date() - start > TIMEOUT_MSEC;
   }
@@ -3316,7 +3313,6 @@ async function run() {
 }
 
 try {
-  validate();
   run().catch(err => { throw err });
 } catch (error) {
   core.setFailed(error.message);
