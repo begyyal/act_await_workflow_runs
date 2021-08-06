9909c19adb0b93ba3a56ceebb103805605e38742
begyyal-ghost <begyyal@gmail.com>
2021-08-06 19:40:27 +0000

    BY-CI

--- dist/index.js
+++ dist/index.js
@@ -3233,41 +3233,56 @@ const axios = __nccwpck_require__(992);
 const WF_NAME = core.getInput('workflowName');
 const TIMEOUT_MSEC = core.getInput('timeoutSec') * 1000;
 const API_PATH = '/repos/' + core.getInput('repos') + '/actions/runs';
+const INTERVAL_SEC = core.getInput('intervalSec');
 
 const RETRY_COUNT = 10;
-const INTERVAL_SEC = 3;
+const STR = {
+  completed: 'completed',
+  queued: 'queued',
+  in_progress: 'in_progress'
+};
 
-function through_(promise) {
-  return promise.catch(err => { throw err });
+function owata_(promise) {
+  return promise.catch(err => {
+    core.setFailed(err.message);
+    process.exit(-1);
+  });
 }
 
-async function getWorkflowIds(status) {
-  const res = await through_(request(API_PATH, newConf(status), RETRY_COUNT));
-  return res.data.workflow_runs
-    .filter(wfr => !WF_NAME || wfr.name == WF_NAME)
-    .map(wfr => wfr.id);
+async function getWorkflowIds() {
+
+  // completed以外でのフィルタリングを嫌気してのこれ
+  // https://github.com/begyyal/act_await_wf_execution/issues/1
+  let runs, allRuns = [], count = 1;
+  do {
+    runs = (await owata_(request(API_PATH, newConf(count++), RETRY_COUNT)))
+      .data.workflow_runs
+      .filter(wfr => wfr.status != STR.completed && (!WF_NAME || wfr.name == WF_NAME));
+    Array.prototype.push.apply(allRuns, runs);
+  } while (runs.length == 30);
+
+  return runs.map(wfr => wfr.id);
 }
 
 async function checkCompletion(ids) {
-  return (await through_(Promise
-    .all(ids.map(id => through_(getWorkflowStatus(id))))))
-    .every(s => s == 'completed');
+  return (await Promise
+    .all(ids.map(id => owata_(getWorkflowStatus(id)))))
+    .every(s => s == STR.completed);
 }
 
 async function getWorkflowStatus(id) {
   const path = API_PATH + '/' + id;
-  const res = await through_(request(path, newConf(), RETRY_COUNT));
-  return res.status;
+  const res = await owata_(request(path, newConf(), RETRY_COUNT));
+  console.log(id + ' - ' + res.data.status);
+  return res.data.status;
 }
 
 async function request(path, config, count) {
 
-  const res = await through_(axios.get(path, config));
+  const res = await owata_(axios.get(path, config));
 
   if (res.status != 200)
     console.log('The http status that is response of Github REST API is not success, it is ' + res.status + '.');
-  else if (res.data.total_count && res.data.total_count != res.data.workflow_runs.length)
-    console.log('The response of Github REST API contains a mismatch between [total_count:' + res.data.total_count + '] and [workflow_runs.length:' + res.data.workflow_runs.length + '].');
   else
     return res;
 
@@ -3275,14 +3290,14 @@ async function request(path, config, count) {
     throw 'Github REST API did not return a valid response while retry count .';
 
   await sleep(0.5);
-  return await through_(request(config, count - 1));
+  return await owata_(request(path, config, count - 1));
 }
 
 function sleep(sec) {
   return new Promise((resolve) => setTimeout(resolve, sec * 1000));
 }
 
-function newConf(status) {
+function newConf(page) {
 
   const conf = {
     baseURL: 'https://api.github.com/',
@@ -3292,24 +3307,22 @@ function newConf(status) {
     }
   };
 
-  if (status)
-    conf.params = { status: status };
+  if (page)
+    conf.params = { page: page };
 
   return conf;
 }
 
 async function run() {
 
-  const ids = (await through_(Promise
-    .all(['queued', 'in_progress'].map(s => through_(getWorkflowIds(s))))))
-    .flatMap(arr => arr);
+  const ids = await owata_(getWorkflowIds());
   if (ids.length == 0)
     return;
 
   let timeoutFlag = false;
   const start = new Date();
 
-  while (!(await through_(checkCompletion(ids))) && !timeoutFlag) {
+  while (!(await owata_(checkCompletion(ids))) && !timeoutFlag) {
     await sleep(INTERVAL_SEC);
     timeoutFlag = new Date() - start > TIMEOUT_MSEC;
   }
@@ -3318,11 +3331,7 @@ async function run() {
     core.setFailed('The workflow runs were not completed/started while timeoutSec.');
 }
 
-try {
-  through_(run());
-} catch (error) {
-  core.setFailed(error.message);
-}
+owata_(run());
 
 })();
 
